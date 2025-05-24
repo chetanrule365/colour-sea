8b8749466c8b151d7ad07a4075b4b38492299e75
chetan <chetanrule365@gmail.com>
2025-05-24 16:03:17 +0530

    update next config

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "export",
-  images: {
-    unoptimized: true,
-  },
+  /* config options here */
+  basePath: "/colour-sea",
+  assetPrefix: "/colour-sea/",
 };
 
 export default nextConfig;
